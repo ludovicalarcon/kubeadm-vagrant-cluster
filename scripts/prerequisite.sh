d28a05ae435462eb6b7bc265007bc84f27c4d0eb
azalax <contact@ludovic-alarcon.com>
2022-05-07 17:30:34 +0200

    fix kubernetes version

--- scripts/prerequisite.sh
+++ scripts/prerequisite.sh
@@ -45,9 +45,9 @@ sudo systemctl start containerd
 sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
-sudo apt-get install -y kubelet \
-                        kubeadm \
-                        kubectl
+sudo apt-get install -y kubelet=$KUBERNETES_VERSION \
+                        kubeadm=$KUBERNETES_VERSION \
+                        kubectl=$KUBERNETES_VERSION
 sudo apt-mark hold kubelet=$KUBERNETES_VERSION kubeadm=$KUBERNETES_VERSION kubectl=$KUBERNETES_VERSION
 
 # Systemd drop-in for containerd
